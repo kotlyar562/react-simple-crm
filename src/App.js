f802d3676bb2617a828ac6ca82f1fd55347c13a5
Andrey Kotlyar <kotlyar562@gmail.com>
2018-10-27 17:39:03 +0300

    Implemented layout and navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, NavLink, Link } from 'react-router-dom'
+
+const HomePage = () => (
+  <div>
+    <div className="start-block">Lorem ipsum...</div>
+  </div>
+)
+const OrdersPage = () => (
+  <div>
+    <div className="start-block">Orders</div>
+  </div>
+)
+const AddOrderPage = () => (
+  <div>
+    <div className="start-block">Add Order</div>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div className="container">
+          <header>
+            <ul className="navigation">
+              <li>
+                <NavLink exact to="/" activeClassName="active">
+                  Start
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/orders" activeClassName="active">
+                  Orders
+                </NavLink>
+              </li>
+              <NavLink to="/add_order" activeClassName="active">
+                Add Order
+              </NavLink>
+            </ul>
+            <Link to="/" className="login">
+              Login
+            </Link>
+            <div className="clearfix" />
+          </header>
+          <div className="content">
+            <Route exact path="/" component={HomePage} />
+            <Route path="/orders" component={OrdersPage} />
+            <Route path="/add_order" component={AddOrderPage} />
+          </div>
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
